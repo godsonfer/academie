625150a75956547de8d4ee573215f62d97bee330
godsonfer <godsonferdinand7@gmail.com>
2023-11-16 03:35:13 +0100

    updating

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,7 @@ export default defineNuxtConfig({
   devtools: { timeline: { enabled: true } },
   nitro: {
     prerender: {
-      // crawlLinks: true,
-      routes: ['/'],
+      routes: ['/index'],
     },
   },
   app: {
